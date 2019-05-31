2b88f695896d33d820725b9a64896f98be4d12c1
Lucas Wanderley <lucaswanderley4@gmail.com>
2019-05-31 12:49:07 -0300

    Sequelize configuration

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'g_node_modulo_2',
+  operatorAliases: false,
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
